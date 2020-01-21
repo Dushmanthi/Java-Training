802fdc74cb17db391df760cb054202de322a53a8
Dushamnthi <madushanimf@gmail.com>
2020-01-21 06:05:05 +0530

    get sorted student who got marks greater than 60, added some examples

---
+++ Stream API/Examples/RankEmployee.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapEmployee {  
+
+    public static void main(String[] args) {
+    
+        List<Employee> employeeList = new ArrayList<>();  
+        
+        employeeList.add(new Employee(110,"Jonny"));  
+        employeeList.add(new Employee(402,"Mike"));  
+        employeeList.add(new Employee(105,"Leena"));  
+        employeeList.add(new Employee(205,"Alice"));  
+        employeeList.add(new Employee(356,"Bob"));   
+        
+        List<Employee> sortedEmployee = employeeList.stream()
+                    .sorted(Comparator.comparing(Employee::getName))
+                    .collect(Collectors.toList());
+
+                    
+        System.out.print(sortedEmployee); 
+                  
+    }  
+}  
