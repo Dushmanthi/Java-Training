c9c5d653e5514c73b585c4b52f7e409fdefd440b
Dushamnthi <madushanimf@gmail.com>
2020-02-11 06:26:56 +0530

    bidirectional mappings(OneToOne,OneToMany,ManyToMany)

--- SpringBootProjects/SB_JPA_Hibernate/EmployeeProject2/src/main/java/com/example/employeeService/controller/EmployeeController.java
+++ SpringBootProjects/SB_JPA_Hibernate/EmployeeProject2/src/main/java/com/example/employeeService/controller/EmployeeController.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import org.apache.tomcat.jni.Address;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employeeService.model.Employee;
@@ -18,32 +20,34 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
-	@RequestMapping("/save")
-	public Employee employeeDetails() {
-		//employee details
-		Employee employee = new Employee();
-		employee.setName("Dushi");
-		employee.setCity("Gampaha");
-		
+//	@RequestMapping("/save")
+//	public Employee employeeDetails() {
+//		//employee details
+//		Employee employee = new Employee();
+//		employee.setName("Dushi");
+//		employee.setCity("Gampaha");
+//		
 		//address details
 //		Address address = new Address();
 //		address.
 //		employee.setAddress(address);
 //		return employee;
-		
-}
-	
-	@RequestMapping("/employee")
-	public Employee saveEmployee(@RequestBody Employee employee) {
-		return employeeService.createEmployee(employee);
-	}
-	
-	@RequestMapping("/allEmployee")
-	public List<Employee> getEmployees() {
-		return employeeService.findEmployee();
-	}
-	
-	
+//}
+	 @RequestMapping(value = "/save", method = RequestMethod.POST)
+	    public Employee save(@RequestBody Employee employee) {
+	        return employeeService.createEmployee(employee);
+	    }
+
+	    @RequestMapping(value = "/findAllEmployees", method = RequestMethod.GET)
+	    public List<Employee> findAll() {
+	        return employeeService.findEmployee();
+	    }
+
+	    @RequestMapping("/findById/{id}")
+	    public Integer findById(@PathVariable("id") Integer id) {
+	        return employeeService.findById(id).getId();
+	    }
+
 	
 
 }
