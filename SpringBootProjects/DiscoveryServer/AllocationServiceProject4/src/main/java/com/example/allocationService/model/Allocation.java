f78d308f2711c3dea9047ce63a1180c66535e792
Dushmanthi <madushanimf@gmail.com>
2020-02-15 16:30:28 +0530

    connect EmployeeService and AllocationService through DiscoveryService

--- SpringBootProjects/DiscoveryServer/AllocationServiceProject4/src/main/java/com/example/allocationService/model/Allocation.java
+++ SpringBootProjects/DiscoveryServer/AllocationServiceProject4/src/main/java/com/example/allocationService/model/Allocation.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 @Entity
 @Data
 public class Allocation {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
+
 	Integer empId;
 	String start;
 	String end;
